6c973240d4fc0b92721c7f14089d15b2eb0616ab
Nagarani07 <nagarani@dbsoltechnologies.com>
2026-04-10 17:40:19 +0530

    to change contact our experts

--- src/components/trainingController.jsx
+++ src/components/trainingController.jsx
@@ -128,7 +128,7 @@ const scrollRight = () => {
    <svg className="w-5 h-5 fill-current text-white dark:text-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"/></svg> 
   </button>
         {/* Sliding Steps */}
-        <div ref={scrollRef} className="overflow-x-auto scrollbar-hide">
+        <div ref={scrollRef} className="overflow-hidden">
 
           <div className="flex gap-6 px-2">
             {steps.map((step, index) => (
