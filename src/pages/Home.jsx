f6e4d0d7a4c0bd433c023ffdae0369dcf56e0aee
Nagarani07 <nagarani@dbsoltechnologies.com>
2026-08-03 10:13:43 +0530

    remove ctasection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,6 @@ import Footer from "../components/Footer";
 import WhyChooseUs from "../components/WhyChooseUs";
 import CompanyStats from "../components/CompanyStats";
 import TrustedClients from "../components/TrustedClient";
-import CTASection from "../components/Ctasection";
 import Testimonials from "../components/Testimonial";
 import ERPImplementationProcess from "../components/ERP";
 import DeliveringTailoredSolutions from "../components/Delivering_Tailored_Solutions";
@@ -30,7 +29,6 @@ export default function Home() {
       <TrustedClients />
       <NewsPage />
       <Testimonials />
-      <CTASection />
       <Footer />
     </>
   );
